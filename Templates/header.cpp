9060dcc8d97f623980281982c4423785672e3d7d
zorax <rajanraj253545@gmail.com>
2025-02-22 23:23:57 +0530

    Add templates

--- Templates/header.cpp
+++ Templates/header.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define fio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define int long long int 
 #define vi vector<int>
+#define vs vector<string>
 #define ivec(arr) for (int i = 0; i < arr.size(); i++) cin >> arr[i];
 #define w(x) int x; cin>>x; while(x--)
 #define pb push_back
